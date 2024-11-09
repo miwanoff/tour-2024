9d5e214105b5bc2692a0e876d705fb5b0f00a234
miwanoff <marina.iva.noffa@gmail.com>
2024-11-09 13:14:19 +0200

    Add output

--- country.php
+++ country.php
@@ -4,6 +4,7 @@ $transport = $_GET['transport'];
 $stars = $_GET["stars"];
 $price = $_GET["price"];
 include "header.php";
+include_once "db.php";
 
 ?>
 <h2>Ви обрали:</h2>
@@ -15,4 +16,20 @@ include "header.php";
 </ul>
 <?php
 //echo "$country $transport" ;
+$tour_keys = [];
+foreach ($tours as $key => $tour) {
+    if ($tour['country'] == $country) {
+        $tour_keys[] = $key;
+    }
+}
+//print_r($tour_keys);
+echo "<h2>Тури</h2>";
+for ($i = 0; $i < count($tour_keys); $i++) {
+    //print_r($tours[$tour_keys[$i]]);
+    $n = $i+1;
+    echo "<h3>".$n."</h3>";
+    foreach ($tours[$tour_keys[$i]] as $key => $value) {
+        echo "$key: $value <br>";
+    }
+}
 include "footer.php";
